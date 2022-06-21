a59a70065f95a61c78f679bb807f78734897f539
nciampelletti <natashat@shaw.ca>
2022-06-20 21:17:00 -0700

    implement update, delete

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -1,23 +1,23 @@
-let database = require("../database");
+let database = require("../database")
 
 let remindersController = {
   list: (req, res) => {
-    res.render("reminder/index", { reminders: database.cindy.reminders });
+    res.render("reminder/index", { reminders: database.cindy.reminders })
   },
 
   new: (req, res) => {
-    res.render("reminder/create");
+    res.render("reminder/create")
   },
 
   listOne: (req, res) => {
-    let reminderToFind = req.params.id;
+    let reminderToFind = req.params.id
     let searchResult = database.cindy.reminders.find(function (reminder) {
-      return reminder.id == reminderToFind;
-    });
+      return reminder.id == reminderToFind
+    })
     if (searchResult != undefined) {
-      res.render("reminder/single-reminder", { reminderItem: searchResult });
+      res.render("reminder/single-reminder", { reminderItem: searchResult })
     } else {
-      res.render("reminder/index", { reminders: database.cindy.reminders });
+      res.render("reminder/index", { reminders: database.cindy.reminders })
     }
   },
 
@@ -27,26 +27,46 @@ let remindersController = {
       title: req.body.title,
       description: req.body.description,
       completed: false,
-    };
-    database.cindy.reminders.push(reminder);
-    res.redirect("/reminders");
+    }
+    database.cindy.reminders.push(reminder)
+    res.redirect("/reminders")
   },
 
   edit: (req, res) => {
-    let reminderToFind = req.params.id;
+    let reminderToFind = req.params.id
     let searchResult = database.cindy.reminders.find(function (reminder) {
-      return reminder.id == reminderToFind;
-    });
-    res.render("reminder/edit", { reminderItem: searchResult });
+      return reminder.id == reminderToFind
+    })
+    res.render("reminder/edit", { reminderItem: searchResult })
   },
 
   update: (req, res) => {
-    // implement this code
+    const Id = req.params.id
+    const { title, description, completed } = req.body
+
+    database.cindy.reminders.map((reminder) => {
+      if (reminder.id == Id) {
+        reminder.title = title
+        reminder.description = description
+        reminder.completed = completed === "true" ? true : false
+      }
+      return reminder
+    })
+
+    res.redirect("/reminders")
   },
 
   delete: (req, res) => {
-    // Implement this code
-  },
-};
+    const Id = req.params.id
+    const removeIndex = database.cindy.reminders.findIndex(
+      (reminder) => reminder.id == Id
+    )
 
-module.exports = remindersController;
+    if (removeIndex >= 0) {
+      database.cindy.reminders.splice(removeIndex, 1)
+    }
+    res.redirect("/reminders")
+  },
+}
+
+module.exports = remindersController
--- database.js
+++ database.js
@@ -1,10 +1,31 @@
 let Database = {
-    cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
-    },
-    alex: {
-        reminders: []
-    } 
+  cindy: {
+    reminders: [
+      { id: 1, title: "abc", description: "abcabc", completed: false },
+      {
+        id: 2,
+        title: "buy book",
+        description: "blalala",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "eat",
+        description: "blalala",
+        completed: false,
+      },
+    ],
+  },
+  alex: {
+    reminders: [
+      {
+        id: 11,
+        title: "alex reminder",
+        description: "blalala",
+        completed: false,
+      },
+    ],
+  },
 }
 
-module.exports = Database;
+module.exports = Database
