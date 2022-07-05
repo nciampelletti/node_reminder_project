38dd61d8cc370400929886ea5102ebd76660555c
nciampelletti <natashat@shaw.ca>
2022-07-04 22:01:01 -0700

    final

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -36,15 +36,4 @@ router.get(
   }
 )
 
-// router.get("/sessions", (req, res) => {
-//   req.sessionStore.sessionModel
-//     .findAll()
-//     .then((sessions) =>
-//       sessions.map((sess) => JSON.parse(sess.dataValues.data))
-//     )
-//     .then((sessions) => {
-//       res.send(sessions)
-//     })
-// })
-
 module.exports = router
